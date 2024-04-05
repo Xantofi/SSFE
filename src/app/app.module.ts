de532f8097f5ac951ff025cba6fcf6f74cfee458
BOSCH Miguel <miguel.perez@ext.soprasteria.com>
2024-04-05 13:43:05 +0200

    Detail module correction 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DetailModule } from './detail/detail.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, DetailModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DetailComponent } from './detail.component';
+import { MainComponent } from './main/main.component';
 import { AsideComponent } from '../detail/aside/aside.component';
 import { PrincipalComponent } from '../detail/principal/principal.component';
 import { StarsComponent } from '../detail/stars/stars.component';
 import { SimilarProductsComponent } from '../detail/similar-products/similar-products.component';
 import { ReviewsComponent } from '../detail/reviews/reviews.component';
 import { SimProductComponent } from '../detail/sim-product/sim-product.component';
-import { MainComponent } from '../detail/main/main.component';
 
 @NgModule({
   declarations: [
-    DetailComponent,
+    MainComponent,
     AsideComponent,
     PrincipalComponent,
     StarsComponent,
     SimilarProductsComponent,
     ReviewsComponent,
     SimProductComponent,
-    MainComponent,
   ],
   imports: [CommonModule],
+  exports: [MainComponent],
 })
 export class DetailModule {}
