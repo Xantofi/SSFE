5dd553daf4343128ee12e7e0c0f94aa032496510
BOSCH Miguel <miguel.perez@ext.soprasteria.com>
2024-04-05 12:59:51 +0200

    Detail module correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AsideComponent } from './detail/aside/aside.component';
-import { PrincipalComponent } from './detail/principal/principal.component';
-import { StarsComponent } from './detail/stars/stars.component';
-import { SimilarProductsComponent } from './detail/similar-products/similar-products.component';
-import { ReviewsComponent } from './detail/reviews/reviews.component';
-import { SimProductComponent } from './detail/sim-product/sim-product.component';
-import { MainComponent } from './detail/main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AsideComponent,
-    PrincipalComponent,
-    StarsComponent,
-    SimilarProductsComponent,
-    ReviewsComponent,
-    SimProductComponent,
-    MainComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DetailComponent } from './detail.component';
+import { AsideComponent } from '../detail/aside/aside.component';
+import { PrincipalComponent } from '../detail/principal/principal.component';
+import { StarsComponent } from '../detail/stars/stars.component';
+import { SimilarProductsComponent } from '../detail/similar-products/similar-products.component';
+import { ReviewsComponent } from '../detail/reviews/reviews.component';
+import { SimProductComponent } from '../detail/sim-product/sim-product.component';
+import { MainComponent } from '../detail/main/main.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    DetailComponent,
+    AsideComponent,
+    PrincipalComponent,
+    StarsComponent,
+    SimilarProductsComponent,
+    ReviewsComponent,
+    SimProductComponent,
+    MainComponent,
+  ],
+  imports: [CommonModule],
 })
-export class DetailModule { }
+export class DetailModule {}
